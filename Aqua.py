584bf2c22b05b93b9d79897db935260c9422ed8d
yaroslav-tsuprak <yaroslav.tsuprak@gmail.com>
2021-06-18 00:03:57 +0700

    Added Raspberry Pi GPIO support

--- Aqua.py
+++ Aqua.py
@@ -1,20 +1,27 @@
+import time
+
 from AquaUtil import AquaUtil
 from Database import Database
+import RPi.GPIO as GPIO
 
 # Time parameters
 lighting_enabled = True
 lighting_start_hours = 9
 lighting_stop_hours = 18
 lighting_start_minutes = 0
+lighting_gpio = 17
 oxygen_enabled = True
 oxygen_start_hours = 9
 oxygen_stop_hours = 22
 oxygen_start_minutes = 0
+oxygen_gpio = 27
 feeding_enabled = True
 feeding_start_hours = 9
 feeding_stop_hours = 22
 feeding_number_of = 2
+feeding_gpio = 22
 # Flags
+debug = True
 food = False
 light = False
 oxygen = False
@@ -34,12 +41,11 @@ def resetAllParameters():
     global feeding_first_state
     count = connect.select_from_db()
     if utils.checkTimeForFeeding(feeding_start_hours, feeding_stop_hours):
+        feeding_first_hour = feeding_start_hours + 1
         if count == 0:
             if feeding_number_of == 1:
-                feeding_first_hour = feeding_start_hours
                 feeding_second_hour = feeding_stop_hours
             elif feeding_number_of == 2:
-                feeding_first_hour = feeding_start_hours + 1
                 feeding_second_hour = utils.getSecondHours(feeding_start_hours, feeding_stop_hours)
         elif count == 1:
             if feeding_number_of == 1:
@@ -51,17 +57,24 @@ def resetAllParameters():
             food = True
 
 
+GPIO.setmode(GPIO.BCM)
 resetAllParameters()
 while True:
     if lighting_enabled:
         lighting_timer = utils.checkTime(lighting_start_hours, lighting_stop_hours, lighting_start_minutes)
         # Disable lighting
         if light and not lighting_timer:
-            print("Lighting - Disabled")
+            if debug:
+                print("Lighting - Disabled")
+            GPIO.output(lighting_gpio, GPIO.LOW)
+            GPIO.setup(lighting_gpio, GPIO.IN)
             light = False
         # Enable lighting
         elif not light and lighting_timer:
-            print("Lighting - Enabled")
+            if debug:
+                print("Lighting - Enabled")
+            GPIO.setup(lighting_gpio, GPIO.OUT)
+            GPIO.output(lighting_gpio, GPIO.HIGH)
             light = True
     else:
         print("Lighting is disabled in config")
@@ -69,20 +82,30 @@ while True:
         oxygen_timer = utils.checkTime(oxygen_start_hours, oxygen_stop_hours, oxygen_start_minutes)
         # Disable oxygen
         if oxygen and not oxygen_timer:
-            print("Oxygen - Disabled")
+            if debug:
+                print("Oxygen - Disabled")
+            GPIO.output(oxygen_gpio, GPIO.LOW)
+            GPIO.setup(oxygen_gpio, GPIO.IN)
             oxygen = False
         # Enable oxygen
         elif not oxygen and oxygen_timer:
-            print("Oxygen - Enabled")
+            if debug:
+                print("Oxygen - Enabled")
+            GPIO.setup(oxygen_gpio, GPIO.OUT)
+            GPIO.output(oxygen_gpio, GPIO.HIGH)
             oxygen = True
     else:
         print("Oxygen is disabled in config")
     if feeding_enabled:
         if not food:
-            if not feeding_first_state and utils.checkHour(
-                    feeding_first_hour) or not feeding_second_state and utils.checkHour(feeding_second_hour):
-                # ENABLE GPIO
-                print("Feeding...")
+            if not feeding_first_state and utils.checkHour(feeding_first_hour) or not feeding_second_state and utils.checkHour(feeding_second_hour):
+                if debug:
+                    print("Feeding...")
+                GPIO.setup(feeding_gpio, GPIO.OUT)
+                GPIO.output(feeding_gpio, GPIO.HIGH)
+                time.sleep(2)
+                GPIO.output(feeding_gpio, GPIO.LOW)
+                GPIO.setup(feeding_gpio, GPIO.IN)
                 connect.save_to_db()
                 count_from_database = connect.select_from_db()
                 if count_from_database == feeding_number_of:
@@ -90,7 +113,13 @@ while True:
                 elif count_from_database == 1 and feeding_number_of > 1:
                     feeding_first_state = True
         elif backup_feeding:
-            print("Backup Feeding...")
+            if debug:
+                print("Backup Feeding...")
+            GPIO.setup(feeding_gpio, GPIO.OUT)
+            GPIO.output(feeding_gpio, GPIO.HIGH)
+            time.sleep(2)
+            GPIO.output(feeding_gpio, GPIO.LOW)
+            GPIO.setup(feeding_gpio, GPIO.IN)
             connect.save_to_db()
             backup_feeding = False
         if utils.checkHour(feeding_start_hours) and food:
